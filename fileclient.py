26a88f4836282d1f30071207d5b0c1000dde8648
pthorne3 <60328456+pthorne3@users.noreply.github.com>
2020-05-07 17:34:54 -0400

    Client and Server TCP Files
    These are the base files I'll be using. No encryption functions added yet...

---
+++ fileclient.py
@@ -0,0 +1,47 @@
+import socket
+import time
+
+
+'''
+*************************************************************
+* Connect to server (on this same machine) using IP address *
+*************************************************************
+'''
+time_to_send_list = []
+for n_times in range(100):
+    
+    s = socket.socket()
+    host_name = socket.gethostname()
+    ip_addr = socket.gethostbyname(host_name)
+    port = 12345
+    
+    print("I am connecting to server side: " +ip_addr)
+    s.connect((ip_addr, port))
+    
+    print("I am sending file 'testfile.txt' for the " +str(n_times +1) +"th time" )            # Note: n_times, using range, goes from 0 thru 2
+    begin_millisecond_time = int(round(time.time() * 1000))
+    
+    f = open('testfile.txt','rb')
+    file_data = f.read()
+    f.close()
+    
+    s.send(file_data)
+    ending_millisecond_time = int(round(time.time() * 1000))
+    print("I am finishing sending file 'testfile.txt' for the " +str(n_times +1) +"th time" )            # Note: n_times, using range, goes from 0 thru 2
+    time_it_took_to_send_in_milliseconds = ending_millisecond_time - begin_millisecond_time
+    print("The time used in millisecond to send file 'testfile.txt' for " +str(n_times+1) +"th time is: ", time_it_took_to_send_in_milliseconds)
+    print()
+    
+    time_to_send_list.append(time_it_took_to_send_in_milliseconds)
+    
+
+    s.shutdown(socket.SHUT_WR)
+    s.close()
+    
+
+print("the average time to send file 'testfile.txt' in milliseconds is: ", sum(time_to_send_list) / len(time_to_send_list))
+
+print("I am done")
+    
+    
+
---
+++ fileserver.py
@@ -0,0 +1,78 @@
+import socket
+import time
+
+'''
+******************************************************************************
+* A copy of the sample file is stored on the server, by default, to be used  *
+* to compare with file being read as the client continues to send the same   *
+* file up to 100 times.                                                      *
+******************************************************************************
+'''
+
+server_side_file = f = open('testfile.txt','rb')
+server_side_sample_data = f.read()
+f.close()
+
+# Add an incrementing number to the file name as each file is received
+file_number = 0
+
+time_to_send_list = []
+total_incorrect_times_received = 0
+total_correct_times_received = 0
+
+s = socket.socket()
+host_name = socket.gethostname()
+ip_addr = socket.gethostbyname(host_name)
+port = 12345
+s.bind((ip_addr, port))
+
+s.listen(5)
+print("I am ready for any client side request")
+print("--------------------------------------")
+while True:
+    c, addr = s.accept()
+    
+    begin_millisecond_time = int(round(time.time() * 1000))
+    file_number = file_number +1
+    
+    print("I am starting receiving file 'testfile.txt' for the ", file_number, 'th time')
+    print('Got connection from', addr)
+    
+    new_server_file_name = 'sentfile_' +str(file_number) +".txt"
+    
+    l = c.recv(4096)
+    print("I am finishing receiving file 'testfile.txt' for the ", file_number, 'th time')
+    
+    new_server_file = open(new_server_file_name, 'wb')
+    new_server_file.write(l)
+    new_server_file.close()
+    
+    if server_side_sample_data == l:
+        print(".. the file just uploaded is the same as the sample file")
+        total_correct_times_received += 1
+    else:
+        print(".. the file uploaded is different from the sample file")
+        total_incorrect_times_received += 1
+    
+    ending_millisecond_time = int(round(time.time() * 1000))
+    time_it_took_to_send_in_milliseconds = ending_millisecond_time - begin_millisecond_time
+    print("The time used in millisecond to receive file 'testfile.txt' for " ,file_number, "th time is: ", time_it_took_to_send_in_milliseconds)
+    print()
+    
+    time_to_send_list.append(time_it_took_to_send_in_milliseconds)
+
+    c.close()
+    
+    if file_number == 100:
+        break
+    
+    print()
+
+s.close()
+
+print("the average time to send file 'testfile.txt' in milliseconds is: ", sum(time_to_send_list) / len(time_to_send_list))
+print("The number of times file received correctly   is: ", total_correct_times_received)
+print("The number of times file received incorrectly is: ", total_incorrect_times_received)
+
+print("I am done")
+
